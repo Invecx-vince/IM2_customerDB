4767243e484f47fd9df09a0e5c7f5b34caf79e1d
Invecx-vince <59422139+Invecx-vince@users.noreply.github.com>
2020-10-06 07:52:19 +0800

    Add files via upload

---
+++ customer/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import *
+
+class CustomerForm(forms.ModelForm):
+
+	class Meta:
+		model = Customer
+		fields = ("name",)
+
+class ProdForm(forms.ModelForm):
+
+	class Meta:
+		model = Product
+		fields = ("name",)
