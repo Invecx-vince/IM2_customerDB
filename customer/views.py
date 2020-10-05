dee6f68c10378ae212bc7bfdb69d899dc2b0a73d
Invecx-vince <59422139+Invecx-vince@users.noreply.github.com>
2020-10-06 07:49:22 +0800

    Update views.py

--- customer/views.py
+++ customer/views.py
@@ -2,7 +2,7 @@ from django.http import Http404
 from django.shortcuts import render,redirect
 from django.views.generic import View
 from django.http import HttpResponse
-from .forms import CustomerForm
+from .forms import CustomerForm, ProdForm
 from .models import *
 
 # Create your views here.
@@ -11,7 +11,8 @@ class BoardView(View):
 		qs_customers = Customer.objects.all()
 		qs_products = Product.objects.all()
 		context = {
-			'customers' : qs_customers
+			'customers' : qs_customers,
+			'products'	: qs_products
 		}
 
 		return render(request,'warehouse/dashboard.html',context)
@@ -70,10 +71,48 @@ class RegisterView(View):
 
 
 
-
 class LandingView(View):
 	def get(self,request):
 		return render(request,'warehouse/NeoSGP_home.html')
 
 	def post(self,request):
 		return render(request,'warehouse/registration.html')
+
+
+class ProdRegView(View):
+	def get(self,request):
+		return render(request,'warehouse/prodregis.html')
+
+	def post(self,request):	
+		form = ProdForm(request.POST,request.FILES)
+		#namaewa = request.POST.get("fullname")
+		#print(form.name)
+		#adres = request.POST.get("addressu")
+		#print(adres)
+		if form.is_valid():
+			
+			namaewa = request.POST.get("name")
+			value = request.POST.get("p_price")
+			pic = request.FILES.get("prodPic")
+			if(request.POST.get("p_isDoz")=="Yes"):
+				doz=True
+			else:
+				doz=False
+			form = Product( name = namaewa , price=value, picture = pic, byDozen = doz)
+			form.save()
+
+			# return HttpResponse('Customer Recorded!!!')
+			return redirect('customer:board_view')
+			# render(request,'warehouse/dashboard.html')
+
+		else:
+			print(form.errors)
+			return HttpResponse('Invalid data')
+
+# def display_prod_pics(request): 
+  
+#     if request.method == 'GET': 
+#         # getting all the objects of hotel. 
+#         prod = Product.objects.all()  
+#         return render((request, 'warehouse/dashboard.html', 
+#                      {'prod_images' : prod})) 
