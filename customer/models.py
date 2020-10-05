cf948743d278c829831817f9489a970bcf235b80
Invecx-vince <59422139+Invecx-vince@users.noreply.github.com>
2020-10-06 07:49:53 +0800

    Update models.py

--- customer/models.py
+++ customer/models.py
@@ -2,23 +2,30 @@ from django.db import models
 from datetime import datetime
 
 # Create your models here.
-class Customer(models.Model):
+class Person(models.Model):
 	name = models.CharField(max_length = 100)
 	address = models.CharField(max_length = 50)
 	birthdate = models.DateField()
+	
+	
+	gender = models.CharField(max_length = 6)
+	
+	class Meta: #optional for not getting default table name which is projectname_ClassName
+		db_table = "Person"
+
+class Customer(Person):
 	status = models.CharField(max_length = 10)
 	#email = models.EmailField()
 	#phone = models.CharField(max_length = 11)
-	gender = models.CharField(max_length = 6)
-	
+
 	class Meta: #optional for not getting default table name which is projectname_ClassName
 		db_table = "Customer"
 
 class Product(models.Model):
 	#email = models.EmailField(default = '', null = False)
 	name = models.CharField(max_length = 100)
-	price = models.FloatField()
-	#picture = models.DateField()
+	price = models.IntegerField()
+	picture = models.ImageField(upload_to='prodImages',blank = True, null = True)
 	byDozen = models.BooleanField()
 	
 	class Meta: #optional for not getting default table name which is projectname_ClassName
