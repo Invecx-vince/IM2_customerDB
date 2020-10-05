49805e53ba051ee5da4d2b217f032c895f9c0283
Invecx-vince <59422139+Invecx-vince@users.noreply.github.com>
2020-10-06 07:47:54 +0800

    Update urls.py

--- customer/urls.py
+++ customer/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
 
 app_name='customer'
 
@@ -7,5 +10,8 @@ app_name='customer'
 urlpatterns = [
     path('board',views.BoardView.as_view(),name='board_view'),
     path('register',views.RegisterView.as_view(),name='register_view'),
-    path('landing',views.LandingView.as_view(),name='landing_view')
+    path('landing',views.LandingView.as_view(),name='landing_view'),
+    path('prodreg',views.ProdRegView.as_view(),name = 'prodreg_view')
 ]
+
+urlpatterns += staticfiles_urlpatterns()
