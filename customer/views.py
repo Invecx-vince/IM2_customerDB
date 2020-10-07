68831eb3a492fec4f9eb2d9fa698db83890ee501
VhynceNarca <71302428+VhynceNarca@users.noreply.github.com>
2020-10-07 21:37:13 +0800

    Update views.py

--- customer/views.py
+++ customer/views.py
@@ -2,7 +2,7 @@ from django.http import Http404
 from django.shortcuts import render,redirect
 from django.views.generic import View
 from django.http import HttpResponse
-from .forms import CustomerForm, ProdForm
+from .forms import CustomerForm, ProdForm, CartForm, OrderForm
 from .models import *
 
 # Create your views here.
@@ -18,7 +18,6 @@ class BoardView(View):
 		return render(request,'warehouse/dashboard.html',context)
 
 	def post(self,request):
-		# return render(request,'warehouse/registration.html')
 		if request.method == 'POST':	
 			if 'btnUpdate' in request.POST:
 				custID = request.POST.get("customer-id")
@@ -71,12 +70,19 @@ class RegisterView(View):
 
 
 
+
 class LandingView(View):
 	def get(self,request):
 		return render(request,'warehouse/NeoSGP_home.html')
 
 	def post(self,request):
-		return render(request,'warehouse/registration.html')
+		if request.method == 'POST':	
+			if 'custom' in request.POST:
+				return render(request,'warehouse/registration.html')
+			elif 'produkto' in request.POST:
+				return redirect('customer:prodreg_view')
+			elif 'Browse' in request.POST:
+				return redirect('customer:products_view')
 
 
 class ProdRegView(View):
@@ -109,6 +115,70 @@ class ProdRegView(View):
 			print(form.errors)
 			return HttpResponse('Invalid data')
 
+
+class ProductView(View):
+	def get(self,request):
+		qs_products = Product.objects.all()
+		qs_cart = Cart.objects.all()
+		context = {
+			'products'	: qs_products,
+			'cart'      : qs_cart
+		}
+		return render(request,'warehouse/Products.html',context)
+
+	def post(self,request):
+		cform = OrderForm(request.POST,request.FILES)
+		if request.method == 'POST':
+			if 'addtocart' in request.POST:
+				if cform.is_valid():
+					namaewa = request.POST.get("product-name")
+					proice = request.POST.get("product-proice")
+					amount = request.POST.get("quantity")
+					cform = Order(name=namaewa,price=proice,quantity = amount)
+					cform.save()
+					return redirect('customer:products_view')
+				else:
+					print(cform.errors)
+					return HttpResponse('Invalid data')
+			elif 'gotocart' in request.POST:
+				return redirect('customer:cart_view')
+			elif 'gohome' in request.POST:
+				return redirect('customer:landing_view')
+
+
+class CartView(View):
+	def get(self,request):
+		qs_products = Product.objects.all()
+		qs_cart = Cart.objects.all()
+		qs_order = Order.objects.all()
+		context = {
+			'products'	: qs_products,
+			'cart'      : qs_cart,
+			'orders'		: qs_order
+		}
+		return render(request,'warehouse/cart.html',context)
+
+	def post(self,request):
+		
+		if request.method == 'POST':	
+			if 'conpirmOrder' in request.POST:
+				itemID = request.POST.get("items-id")
+				#itemname = request.POST.get("items-name")
+				#itemprice = request.POST.get("items-price")
+				itemquantity = request.POST.get("items-quantity")
+				orderDate = request.POST.get("orderdate")
+				orderLocation = request.POST.get("location")
+				final = Order.objects.filter(cart_ptr_id = itemID).update(quantity =itemquantity,orderdate = orderDate, location = orderLocation)
+				#form = Order( name=itemname,price=itemprice,quantity=itemquantity,cart_ptr_id=itemID, orderdate = orderDate, location = orderLocation)
+				#form.save()
+				print('order received')
+			elif 'cancelOrder' in request.POST:
+				orderID = request.POST.get("del-targetID")
+				cusTard = Order.objects.filter(cart_ptr_id = orderID).delete()
+				personbye = Cart.objects.filter(id = orderID).delete()
+				print(orderID)
+				print('Order is deleted')
+		return redirect('customer:cart_view')
 # def display_prod_pics(request): 
   
 #     if request.method == 'GET': 
